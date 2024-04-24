582c5f48a9aa9fb3db18fb4a9a62aa3d1d00a69c
Alex <alexpeters59@gmail.com>
2024-04-24 11:44:29 -0500

    Home page updates

--- Peters-Website/src/Components/Home.jsx
+++ Peters-Website/src/Components/Home.jsx
@@ -1,16 +1,49 @@
 import "../Styles/home.css"
+import { useState } from "react"
 
-function Home () {
-    
+function Home() {
+    const [showDetails, setShowDetails] = useState({})
 
+    const toggleDetails = (pillar) => {
+        setShowDetails(prevState => ({
+            ...prevState,
+            [pillar]: !prevState[pillar]
+        }));
+    };
 
     return (
         <div className="Home">
-            <img src=""></img>
-            <header> Welcome to where innovation and Design meets Human Resources </header>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt amet assumenda recusandae animi voluptatibus quos ab accusantium asperiores veritatis officia dolore laboriosam tenetur, magnam maxime quae repudiandae facilis quibusdam id. </p>
+            <header className="HeaderHome"> “The technology you use impresses no one. The experience you create with it is everything.” - Sean Gerety </header>
+            <img className="img1" src="src/Assets/ales-nesetril-Im7lZjxeLhg-unsplash.jpg" /><br></br>
+            <img className="img2" src="src/Assets/HeadShot.png" />
+            <p className="imgtext">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt amet assumenda recusandae animi voluptatibus quos ab accusantium asperiores veritatis officia dolore laboriosam tenetur, magnam maxime quae repudiandae facilis quibusdam id. </p>
+            <section className="Pillars">
+                <h1 className="h1Home">What continues to inspire me </h1>
+                <div className="homeDiv">
+                    <img className="img1home" src="https://picsum.photos/300"/> 
+                    <button className="homeButton" onClick={() => toggleDetails('People')}>People</button>
+                    {showDetails['People'] && (
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, facilis at doloremque fugiat maxime rem praesentium quae! At, sapiente repellendus optio in omnis iste dolorem veritatis, molestiae molestias, commodi neque?</p>
+                        )}
+                </div>
+                <div className="homeDiv">
+                    <img className="img3home" src="https://picsum.photos/300"/> <br></br>
+                    <button className="buttom1homeimg3" onClick={() => toggleDetails('engineering')}> Engineering</button>
+                    {showDetails['engineering'] && (
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit maiores temporibus deleniti distinctio, veritatis est tenetur nostrum consequuntur provident! Voluptatibus excepturi consequuntur quia eveniet minima quidem facilis. Illo, laborum earum.</p>
+                    )};
+                </div>
+                <div className="homeDiv">
+                    <img className="img3home" src="https://picsum.photos/300"/> 
+                    <button className="buttom1homeimg3" onClick={() => toggleDetails('data')}> Data</button>
+                    {showDetails['data'] && (
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit maiores temporibus deleniti distinctio, veritatis est tenetur nostrum consequuntur provident! Voluptatibus excepturi consequuntur quia eveniet minima quidem facilis. Illo, laborum earum.</p>
+                    )};
+                </div>
 
+            </section>
         </div>
-    )   
+            
+    )
 }
 export default Home
--- Peters-Website/src/Components/NavBar.jsx
+++ Peters-Website/src/Components/NavBar.jsx
@@ -1,16 +1,10 @@
 import { Link } from "react-router-dom";
-import { List } from "phosphor-react";
 import {  } from "phosphor-react";
-import { useState } from "react";
 import "../Styles/navbar.css";
 
 
 function Navbar() {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
 
 
     return (
@@ -20,19 +14,13 @@ function Navbar() {
                     <Link to="/" className="navbar-logo">
                         <img className="navbar-logo-img" src="src/Assets/vecteezy_ap-letter-circle-logo-design-with-gold-color_9751044-1.jpg" />
                     </Link>
-                    <div className="menu-icon" onClick={toggleDropdown}> 
-                    <List />
-                    </div>
-                    {isDropdownOpen && (
-                        <ul className="nav-menu-dropdown">
-                            <li className="nav-item">
-                                <Link to="/Resume" className="nav-links" onClick={() => setIsDropdownOpen(false)}>Resume</Link>
-                                <Link to="/Portfolio" className="nav-links" onClick={() => setIsDropdownOpen(false)}>Portfolio</Link>
-                                <Link to="/Contact" className="nav-links" onClick={() => setIsDropdownOpen(false)}>Contact</Link>
-                                <Link to="/Home" className="nav-links" onClick={() => setIsDropdownOpen(false)}>Home</Link>
-                            </li>
-                        </ul>
-                    )}
+                    <Link to="/" className="nav-links">Home</Link>
+                    <Link to="/Resume" className="nav-links">Resume</Link> 
+                    <Link to="/Portfolio" className="nav-links">Portfolio</Link> 
+                    <Link to="/Contact" className="nav-links" >Contact</Link> 
+                    <Link to="/" className="navbar-logo">
+                        <img className="navbar-logo-img2" src="src/Assets/vecteezy_ap-letter-circle-logo-design-with-gold-color_9751044-1.jpg" />
+                    </Link>
                 </div>
             </nav>
         </>
