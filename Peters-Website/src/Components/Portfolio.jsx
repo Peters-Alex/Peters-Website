6a37077ff72121f4709c7b9fb1a85a16aee0a339
Alex <alexpeters59@gmail.com>
2024-04-26 21:17:34 -0500

    updates to portfolio

--- Peters-Website/src/Components/Portfolio.jsx
+++ Peters-Website/src/Components/Portfolio.jsx
@@ -1,14 +1,37 @@
-function Portfolio  () {
-    
+function Portfolio() {
+
 
 
     return (
         <div className="Portfolio">
             <img src=""></img>
-            <header> Welcome to where innovation and Design meets Human Resources </header>
+            <h1 className="header1"> Tools </h1>
+            <h2 className="header2"> Software</h2>
+            <ul className="SystemTools">
+                <li> This website is hosted on <a href="https://www.netlify.com/"> Netlify</a></li>
+                <li>Coding: <a href="https://code.visualstudio.com/">Visual Studio Code</a></li>
+                <li>Notes: <a href="https://obsidian.md/">Obsidian</a> </li>
+            </ul>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt amet assumenda recusandae animi voluptatibus quos ab accusantium asperiores veritatis officia dolore laboriosam tenetur, magnam maxime quae repudiandae facilis quibusdam id. </p>
-
+            <div className="DevPro">
+                <h3 className="header3"> Development Process</h3>
+                <img className="WireFrame"src="" alt=""></img>
+                <p className="wireFrameTxt"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, aliquid. Aspernatur animi labore, itaque ipsum soluta modi porro necessitatibus fuga ratione molestiae distinctio repellendus, praesentium, cumque sed qui maxime quidem?</p>
+                <h4>Workflow</h4>
+                <img className="WorkFlow" src="" alt=""></img>
+                <p className="WorkFlowTxt">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum pariatur ullam error delectus animi velit explicabo dolores quidem laudantium. Officia iste officiis explicabo amet asperiores non labore saepe veniam laboriosam!</p>
+            </div>
+            <div className="card anchored">
+                <div>
+                    <time>2024</time>
+                    <a className="cardHeader" href="https://github.com/Peters-Alex/E-Commerce">alexpeters.com</a>
+                    <p>The source of this website</p>
+                    
+                </div>
+    
+            </div>
         </div>
-    )   
+
+    )
 }
 export default Portfolio
--- Peters-Website/src/Components/Resume.jsx
+++ Peters-Website/src/Components/Resume.jsx
@@ -1,16 +1,33 @@
 import {Document, Page, pdfjs} from 'react-pdf';
 import APResume from '../Assets/APResume.pdf';
+import '../Styles/resume.css'
+import { useState } from 'react'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 function Resume () {
-    
+    // const [numPages, setNumPages] = useState()
+    // const onDocumentLoadSuccess = ({ numPages }) => {
+    //     setNumPages(numPages);
+    // }
+
 
 
     return (
         <div className="Resume">
+            <img src=""></img>
+            <header> Welcome to where innovation and Design meets Human Resources </header>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt amet assumenda recusandae animi voluptatibus quos ab accusantium asperiores veritatis officia dolore laboriosam tenetur, magnam maxime quae repudiandae facilis quibusdam id. </p>
 
-
-           
         </div>
-    )   
+     
+            // {/* <Document className="resumeimg" file={APResume} onLoadSuccess={onDocumentLoadSuccess}
+            //       onLoadError={console.error}>
+            //         {Array.from(new Array(numPages), (el, index) => (
+            //         <Page key={`page_${index + 1}`} pageNumber={index + 1} renderTextLayer={false} renderAnnotationLayer={false}/>
+            //     ))} */}
+            // {/* </Document> */}
+            
+        
+    );   
 }
 export default Resume
